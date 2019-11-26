e5a115c83548cab79431bd2ee377183b8e3f9b32
zrwang <zrwang@peckshield.com>
2019-11-26 14:58:05 +0800

    add loading

---
+++ src/components/loading.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="loading">
+    <div class="container">
+      <div class="loader loader-7">
+        <div class="line line1"></div>
+        <div class="line line2"></div>
+        <div class="line line3"></div>
+        <div class="load_text">loading&nbsp;...</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "loading"
+    }
+</script>
+
+<style scoped>
+  .loading {
+    height: 100%;
+    background: rgba(247, 249, 250, 1);
+  }
+
+  .container {
+    text-align: center;
+    width: 160px;
+    height: 60px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
+  }
+
+  .loader {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+
+  .loader-star {
+    position: absolute;
+    top: calc(50% - 12px);
+  }
+
+  .loader-7 .line {
+    width: 8px;
+    position: absolute;
+    border-radius: 5px;
+    bottom: 0; 
+    /*background: -webkit-gradient(linear, left top, left bottom, from(#1ee95d), to(#5714ce));
+            background: -webkit-linear-gradient(top, #1ee95d, #5714ce);
+            background: linear-gradient(to bottom, #1ee95d, #5714ce);*/
+  }
+
+  .loader-7 .line1 {
+    left: 0;
+    -webkit-animation: line-grow 0.5s ease alternate infinite;
+    animation: line-grow 0.5s ease alternate infinite;
+    background: rgba(9, 189, 210, 1);
+  }
+
+  .loader-7 .line2 {
+    left: 20px;
+    -webkit-animation: line-grow 0.5s 0.2s ease alternate infinite;
+    animation: line-grow 0.5s 0.2s ease alternate infinite;
+    background: rgba(6, 158, 248, 1);
+  }
+
+  .loader-7 .line3 {
+    left: 40px;
+    -webkit-animation: line-grow 0.5s 0.4s ease alternate infinite;
+    animation: line-grow 0.5s 0.4s ease alternate infinite;
+    background: rgba(38, 112, 255, 1);
+  }
+
+  .load_text {
+    position: absolute;
+    top: 30px;
+    left: 80px;
+    font-size: 24px;
+    font-weight: 500;
+    color: rgba(46, 50, 51, 1);
+  }
+
+  @-webkit-keyframes line-grow {
+    0% {
+      height: 0;
+    }
+    100% {
+      height: 75%;
+    }
+  }
+
+  @keyframes line-grow {
+    0% {
+      height: 0;
+    }
+    100% {
+      height: 75%;
+    }
+  }
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,23 +1,75 @@
 <template>
   <div>
     <head-top></head-top>
-    index
+    <div class="index">
+      <p>index</p>
+      <el-button
+      type="primary"
+      @click="openFullScreen"
+      v-loading.fullscreen.lock="fullScreenLoading">
+        指令方式
+      </el-button>
+
+
+      <el-button
+      type="primary"
+      @click="openFullScreen1">
+        服务方式
+      </el-button>
+
+    </div>
     <footer-bottom></footer-bottom>
   </div>
 </template>
 
 <script>
-  import headTop from '../components/top'
-  import footerBottom from '../components/bottom'
-  export default {
-    name: "index",
-    components:{
-      headTop,
-      footerBottom
+    import headTop from '../components/top'
+    import footerBottom from '../components/bottom'
+
+    export default {
+        name: "index",
+        data() {
+            return {
+                fullScreenLoading: false
+            }
+        },
+        components: {
+            headTop,
+            footerBottom
+        },
+        mounted() {
+          this.openFullScreen()
+        },
+        methods:{
+            openFullScreen(){
+                let that = this
+                that.fullScreenLoading = true
+                setTimeout(()=>{
+                    that.fullScreenLoading = false
+                },4000)
+            },
+            openFullScreen1(){
+                const loading = this.$loading({
+                    lock:true,
+                    text:'玩命加载中...',
+                    spinner:'el-icon-loading',
+                    background:'rgba(0,0,0,0.8)'
+                })
+                setTimeout(()=>{
+                    loading.close()
+                },4000)
+            }
+        }
     }
-  }
 </script>
 
 <style scoped>
-
+p{
+  height: 100px;
+  display: -webkit-flex;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 40px;
+}
 </style>
